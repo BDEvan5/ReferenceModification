7b283daa2866245ce944037488739bc496e30fd3
Brian Evans <ebrian101@gmail.com>
2021-05-12 15:45:11 +0100

    Modified setup.py: added dependencies and email
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.1'
 DESCRIPTION = 'Testing the Reference Modification Planner'
 LONG_DESCRIPTION = 'Code for accompanying paper on ref mod'
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="ReferenceModification", 
-        version=VERSION,
-        author="Benjamin Evans",
-        author_email="<youremail@email.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=[], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'autonomous racing'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 3",
-            "Operating System :: Linux",
-        ]
+    name="ReferenceModification",
+    version=VERSION,
+    author="Benjamin Evans",
+    author_email="19811799@sun.ac.za",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['torch==1.8.1', 'numpy==1.19.5', 'matplotlib==3.4.1', 'casadi==3.5.5',
+                      'numba==0.53.1', 'scipy==1.6.3'],
+    keywords=['obstacle avoidance', 'autonomous racing', 'reinforcement learning'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ]
 )
